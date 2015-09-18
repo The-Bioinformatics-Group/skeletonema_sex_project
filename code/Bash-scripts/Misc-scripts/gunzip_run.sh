c0bfd189141abf40ba1752f0ad9c3b911f17d1f6
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-09-18 10:57:15 +0200

    script to extract all .gz files using gunzip on sandbox

---
+++ code/Bash-scripts/Misc-scripts/gunzip_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -cwd
+#$ -q sandbox
+#$ -o stdout_gunzip.txt
+#$ -e stderr_gunzip.txt
+#$ -j y
+#$ -S /bin/bash
+
+gunzip /nobackup/data5/skeletonema_sex_project/data/C6D2PACXX_*gz
+
+wait
+echo "Done with script" $DATE
+
+
+
