7dcdd12c3a6a24bd1d73bd3adfe99d1040f83c11
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-09-28 14:02:51 +0200

    changed trinity_run.sh

--- code/Bash-scripts/Trinity-bash-scripts/trinity_run.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_run.sh
@@ -5,10 +5,12 @@
 #$ -e stderr_trinity.txt
 #$ -j y
 #$ -S /bin/bash
+#$ -l mem_free 100
 
 wait 
 
-?
+Trinity --seqType fq --JM 20G --single /state/partition2/mathias/C6D2PACXX-All.txt
+ --CPU 6 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/data/trinity_assembly
 
 wait
 echo "Done with script" 
