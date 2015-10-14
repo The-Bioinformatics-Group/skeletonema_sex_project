f01dfc4358688e41f51c2038bb41fa2e05836f89
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-14 19:22:37 +0200

    Changed script to run single-end assembly on filtered data

--- code/Bash-scripts/Trinity-bash-scripts/trinity_run.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test3/stdout_trinity.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test3/stderr_trinity.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly/stdout_trinity.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly/stderr_trinity.txt
 #$ -j y
 #$ -S /bin/bash
 #$ -l mem_free=100G
 wait 
 
-Trinity --seqType fq --JM 20G --single /nobackup/data5/skeletonema_sex_project/data/rna-sex-data/RNA-sex_all_raw.txt --normalize_max_read_cov 50 --CPU 6 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test3
+Trinity --seqType fq --JM 20G --single /nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex/RNA-sex_all.fq --CPU 8 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly
 
 wait
 echo "Done with script" 
