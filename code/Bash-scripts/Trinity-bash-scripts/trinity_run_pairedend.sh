78611f02f7555837dcb9b5b0296e7156682c81be
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-15 10:34:46 +0200

    Corrected error with duplicated --right

--- code/Bash-scripts/Trinity-bash-scripts/trinity_run_pairedend.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_run_pairedend.sh
@@ -9,7 +9,7 @@
 wait
 LOC_PAIR=/nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results
 wait 
-Trinity --seqType fq --JM 20G --SS_lib_type FR --left ${LOC_PAIR}/1F_fastq-q-filt.txt,${LOC_PAIR}/2F_fastq-q-filt.txt --right --right ${LOC_PAIR}/1R_fastq-q-filt.txt,${LOC_PAIR}/2R_fastq-q-filt.txt --CPU 8 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly
+Trinity --seqType fq --JM 20G --SS_lib_type FR --left ${LOC_PAIR}/1F_fastq-q-filt.txt,${LOC_PAIR}/2F_fastq-q-filt.txt --right ${LOC_PAIR}/1R_fastq-q-filt.txt,${LOC_PAIR}/2R_fastq-q-filt.txt --CPU 8 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly
 wait
 echo "Done with script" 
 date
