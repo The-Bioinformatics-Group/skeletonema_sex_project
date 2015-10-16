e55fb7e9cc0984ec7d8d075f8e01a0b90d17f7f0
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-16 14:04:36 +0200

    Added coordsort_bam parameter which produces a sorted BAM file

--- code/Bash-scripts/Trinity-bash-scripts/trinity_RSEM_a-ev.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_RSEM_a-ev.sh
@@ -9,7 +9,7 @@
 
 wait 
 
-/usr/local/bin/trinityrnaseq_r20140717/util/align_and_estimate_abundance.pl --transcripts /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/trinity_out_dir/Trinity.fasta --seqType fq --single /nobackup/data5/skeletonema_sex_project/data/rna-sex-data/CL10.txt --est_method RSEM --aln_method bowtie --trinity_mode --output_dir /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/
+/usr/local/bin/trinityrnaseq_r20140717/util/align_and_estimate_abundance.pl --transcripts /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/trinity_out_dir/Trinity.fasta --seqType fq --single /nobackup/data5/skeletonema_sex_project/data/rna-sex-data/CL10.txt --est_method RSEM --coordsort_bam --aln_method bowtie --trinity_mode --output_dir /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/
 
 
 wait
