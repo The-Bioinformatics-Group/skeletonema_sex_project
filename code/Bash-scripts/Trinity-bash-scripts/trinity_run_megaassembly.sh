5f1228b8b2039b186761f754d70a6d236d6f835f
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-18 17:43:09 +0200

    Changed some things to try and make the assembly quicker

--- code/Bash-scripts/Trinity-bash-scripts/trinity_run_megaassembly.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_run_megaassembly.sh
@@ -5,13 +5,13 @@
 #$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly/stderr_trinity.txt
 #$ -j y
 #$ -S /bin/bash
-#$ -l mem_free=100G
+
 
 wait
 LOC_SINGLE=/nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex
 LOC_PAIR=/nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results
 wait 
-Trinity --seqType fq --JM 20G --left ${LOC_PAIR}/1F_fastq-q-filt.txt,${LOC_PAIR}/2F_fastq-q-filt.txt,${LOC_SINGLE}/RNA-sex_all.fq --right ${LOC_PAIR}/1R_fastq-q-filt.txt,${LOC_PAIR}/2R_fastq-q-filt.txt --CPU 8 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly
+Trinity --max_memory 240G --bflyHeapSpaceInit 10G --bflyHeapSpaceMax 12G --seqType fq --JM 240G --normalize_max_read_cov --left ${LOC_PAIR}/1F_fastq-q-filt.txt,${LOC_PAIR}/2F_fastq-q-filt.txt,${LOC_SINGLE}/RNA-sex_all.fq --right ${LOC_PAIR}/1R_fastq-q-filt.txt,${LOC_PAIR}/2R_fastq-q-filt.txt --CPU 24 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly
 
 wait
 echo "Done with Trinity assembly!"
