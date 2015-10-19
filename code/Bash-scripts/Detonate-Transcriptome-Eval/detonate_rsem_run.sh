dc4834eaefdca059a1a0f5813dadfec562891cb7
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-19 16:22:57 +0200

    Fixed some things, it runs now

--- code/Bash-scripts/Detonate-Transcriptome-Eval/detonate_rsem_run.sh
+++ code/Bash-scripts/Detonate-Transcriptome-Eval/detonate_rsem_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #$ -cwd
 #$ -q sandbox
-#$ -o 
-#$ -e 
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/stdout_detonate.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/stdout_detonate.txt
 #$ -j y
 #$ -S /bin/bash
 
 wait 
-~/home/mathias/detonate-1.10/rsem-eval/rsem-eval-calculate-score /nobackup/data5/skeletonema_sex_project/data/rna-sex-data/RNA-sex_all_raw.txt /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/trinity_out_dir/Trinity.fasta test_assembly_1 --temporary-folder /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/detonate-temp/
+rsem-eval-calculate-score /nobackup/data5/skeletonema_sex_project/data/rna-sex-data/RNA-sex_all_raw.txt /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/trinity_out_dir/Trinity.fasta test_assembly_1 51 -p 4 --temporary-folder /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/detonate-temp/
 wait
 echo "Done with script" 
 date
