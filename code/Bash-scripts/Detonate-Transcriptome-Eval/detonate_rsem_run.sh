ae68bfb651b9536a77eaec329031a16c79a0c7a8
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-20 16:11:19 +0200

    Fixed assembly path

--- code/Bash-scripts/Detonate-Transcriptome-Eval/detonate_rsem_run.sh
+++ code/Bash-scripts/Detonate-Transcriptome-Eval/detonate_rsem_run.sh
@@ -7,7 +7,7 @@
 #$ -S /bin/bash
 
 wait 
-rsem-eval-calculate-score /nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex/RNA-sex_all.fq /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly/trinity_out_dir/Trinity.fasta single_end-assembly 51 -p 4 --temporary-folder /nobackup/data5/skeletonema_sex_project/test/assembly-test/detonate-eval/single-end_assembly/detonate-temp_singleend/
+rsem-eval-calculate-score /nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex/RNA-sex_all.fq /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly/Trinity.fasta single_end-assembly 51 -p 4 --temporary-folder /nobackup/data5/skeletonema_sex_project/test/assembly-test/detonate-eval/single-end_assembly/detonate-temp_singleend/
 wait
 echo "Done with script" 
 date
