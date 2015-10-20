47a76e79e8e328d2175ce599b42863a995289593
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-20 16:09:14 +0200

    Changed script to run for singleendassembly

--- code/Bash-scripts/Detonate-Transcriptome-Eval/detonate_rsem_run.sh
+++ code/Bash-scripts/Detonate-Transcriptome-Eval/detonate_rsem_run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #$ -cwd
-#$ -q sandbox
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/stdout_detonate.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/stdout_detonate.txt
+#$ -q high_mem
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/detonate-eval/single-end_assembly/stdout_detonate.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/detonate-eval/single-end_assembly/stdout_detonate.txt
 #$ -j y
 #$ -S /bin/bash
 
 wait 
-rsem-eval-calculate-score /nobackup/data5/skeletonema_sex_project/data/rna-sex-data/RNA-sex_all_raw.txt /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/trinity_out_dir/Trinity.fasta test_assembly_1 51 -p 4 --temporary-folder /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/detonate-temp/
+rsem-eval-calculate-score /nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex/RNA-sex_all.fq /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly/trinity_out_dir/Trinity.fasta single_end-assembly 51 -p 4 --temporary-folder /nobackup/data5/skeletonema_sex_project/test/assembly-test/detonate-eval/single-end_assembly/detonate-temp_singleend/
 wait
 echo "Done with script" 
 date
-
-
 
