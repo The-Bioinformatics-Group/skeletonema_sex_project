1d4fb1e89e5223d5137e708e374110a0f49f7952
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-21 12:01:10 +0200

    changed script to run with readnormalization 40 coverage threshold

--- code/Bash-scripts/Trinity-bash-scripts/trinity_run.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly/stdout_trinity.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly/stderr_trinity.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly_readnorm/stdout_trinity.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly_readnorm/stderr_trinity.txt
 #$ -j y
 #$ -S /bin/bash
-#$ -l mem_free=100G
+#$ -l mem_free=120G
 wait 
 
-Trinity --seqType fq --JM 20G --single /nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex/RNA-sex_all.fq --CPU 8 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly
+Trinity --seqType fq --JM 120G --single /nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex/RNA-sex_all.fq --bflyHeapSpaceInit 10G --bflyHeapSpaceMax 12G --CPU 12 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly_readnorm --normalize_max_read cov 40
 
 wait
 echo "Done with script" 
