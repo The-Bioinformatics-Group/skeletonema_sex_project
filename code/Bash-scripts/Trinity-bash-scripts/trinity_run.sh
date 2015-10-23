c33bd3f86fd4818ba4a6f7ac44746ca8290b6d88
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-23 11:49:13 +0200

    Running latest Trinity on singleend-data with readnorm 40

--- code/Bash-scripts/Trinity-bash-scripts/trinity_run.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly_readnorm/stdout_trinity.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly_readnorm/stderr_trinity.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly_readnorm_trinity/stdout_trinity.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly_readnorm_trinity/stderr_trinity.txt
 #$ -j y
 #$ -S /bin/bash
 #$ -l mem_free=120G
+
 wait 
-
-Trinity --seqType fq --JM 120G --single /nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex/RNA-sex_all.fq --bflyHeapSpaceInit 10G --bflyHeapSpaceMax 12G --CPU 12 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly_readnorm --normalize_max_read cov 40
-
+/usr/local/bin/trinityrnaseq-2.1.1/Trinity --seqType fq --max_memory 120G --single /nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex/RNA-sex_all.fq --bflyHeapSpaceInit 10G --bflyHeapSpaceMax 12G --CPU 12 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/single_end-assembly_readnorm_trinity --normalize_max_read_cov 40
 wait
 echo "Done with script" 
 date
