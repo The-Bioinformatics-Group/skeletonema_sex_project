45c0a763d23c9b0a3444bac759311ee85c2a2b32
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-23 12:13:23 +0200

    Running latest Trinity, and with 40 read normalization

--- code/Bash-scripts/Trinity-bash-scripts/trinity_run_megaassembly.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_run_megaassembly.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly/stdout_trinity.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly/stderr_trinity.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity/stdout_trinity.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity/stderr_trinity.txt
 #$ -j y
 #$ -S /bin/bash
-#$ -l mem_free=144G
+#$ -l mem_free=130G
 
 wait
 LOC_SINGLE=/nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex
 LOC_PAIR=/nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results
 wait 
-Trinity --bflyHeapSpaceInit 12G --bflyHeapSpaceMax 14G --seqType fq --JM 144G --left ${LOC_PAIR}/1F_fastq-q-filt.txt,${LOC_PAIR}/2F_fastq-q-filt.txt,${LOC_SINGLE}/RNA-sex_all.fq --right ${LOC_PAIR}/1R_fastq-q-filt.txt,${LOC_PAIR}/2R_fastq-q-filt.txt --CPU 12 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly
+Trinity --bflyHeapSpaceInit 10G --bflyHeapSpaceMax 12G --seqType fq --max_memory 130G --left ${LOC_PAIR}/1F_fastq-q-filt.txt,${LOC_PAIR}/2F_fastq-q-filt.txt,${LOC_SINGLE}/RNA-sex_all.fq --right ${LOC_PAIR}/1R_fastq-q-filt.txt,${LOC_PAIR}/2R_fastq-q-filt.txt --CPU 13 --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity --normalize_max_read_cov 40
 
 wait
 echo "Done with Trinity assembly!"
