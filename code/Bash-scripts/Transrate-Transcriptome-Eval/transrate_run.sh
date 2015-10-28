4884625897faefb8a0643fd584924e375148e284
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-28 17:44:37 +0100

    changed paired end data file-endings due to transrate requirement

--- code/Bash-scripts/Transrate-Transcriptome-Eval/transrate_run.sh
+++ code/Bash-scripts/Transrate-Transcriptome-Eval/transrate_run.sh
@@ -8,7 +8,7 @@
 wait
 LOC_PAIR=/nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results 
 wait
-transrate --assembly /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/Trinity.fasta --left ${LOC_PAIR}/1F_fastq-q-filt.txt,${LOC_PAIR}/2F_fastq-q-filt.txt --right ${LOC_PAIR}/1R_fastq-q-filt.txt,${LOC_PAIR}/2R_fastq-q-filt.txt --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/transrate-eval/paired-end_assembly/
+transrate --assembly /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/Trinity.fasta --left ${LOC_PAIR}/1F_fastq-q-filt.fq,${LOC_PAIR}/2F_fastq-q-filt.fq --right ${LOC_PAIR}/1R_fastq-q-filt.fq,${LOC_PAIR}/2R_fastq-q-filt.fq --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/transrate-eval/paired-end_assembly/
 wait
 echo "Done with script" 
 date
