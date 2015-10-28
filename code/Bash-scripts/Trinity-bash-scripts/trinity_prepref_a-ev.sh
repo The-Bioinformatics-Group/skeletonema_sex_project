13bfeda23777b7108d19d19080d5dea66a473915
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-10-28 13:38:39 +0100

    updated to prepref for paired_end_assembly

--- code/Bash-scripts/Trinity-bash-scripts/trinity_prepref_a-ev.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_prepref_a-ev.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #$ -cwd
 #$ -q sandbox
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/stdout_trin_prepref.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/stderr_trin_prepref.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/align_and_estimate_abundance/stdout_trin_prepref.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/align_and_estimate_abundance/stderr_trin_prepref.txt
 #$ -j y
 #$ -S /bin/bash
 
 
 wait 
 
-/usr/local/bin/trinityrnaseq_r20140717/util/align_and_estimate_abundance.pl --transcripts /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/trinity_out_dir/Trinity.fasta --est_method RSEM --aln_method bowtie --trinity_mode --prep_reference --output_dir /nobackup/data5/skeletonema_sex_project/test/assembly-test/assembly_test1/
+/usr/local/bin/trinityrnaseq_r20140717/util/align_and_estimate_abundance.pl --transcripts /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/Trinity.fasta --est_method RSEM --aln_method bowtie --trinity_mode --prep_reference --output_dir /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/align_and_estimate_abundance/
 
 
 wait
