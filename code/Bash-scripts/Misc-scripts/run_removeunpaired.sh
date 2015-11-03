43ce462d95c34778dda30ab4b8b5df4e3f2d7b0b
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-11-03 12:57:20 +0100

    Running a different script with the same intent

--- code/Bash-scripts/Misc-scripts/run_removeunpaired.sh
+++ code/Bash-scripts/Misc-scripts/run_removeunpaired.sh
@@ -9,7 +9,7 @@
 wait
 echo "Starting removing unpaired paired end reads perl script"
 wait
-/nobackup/data5/skeletonema_sex_project/code/Perl-scripts/removeunpairedreads.pl /nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results/1F_fastq-q-filt.fq /nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results/1R_fastq-q-filt.fq
+/nobackup/data5/skeletonema_sex_project/code/Perl-scripts/removeunpairedreads_anotherscript.pl /nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results/1F_fastq-q-filt.fq /nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results/1R_fastq-q-filt.fq
 wait
 echo "Done with script
 date
