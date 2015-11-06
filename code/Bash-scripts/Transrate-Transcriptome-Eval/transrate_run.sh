7874470e6f30028e913ddd892ff2dcfd0f55cdd5
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-11-06 12:10:41 +0100

    Running transrate on paired end assembly

--- code/Bash-scripts/Transrate-Transcriptome-Eval/transrate_run.sh
+++ code/Bash-scripts/Transrate-Transcriptome-Eval/transrate_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -q high_mem
+#$ -q node0
 #$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/transrate-eval/paired-end_assembly/stdout_transrate.txt
 #$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/transrate-eval/paired-end_assembly/stderr_transrate.txt
 #$ -j y
@@ -8,7 +8,7 @@
 wait
 LOC_PAIR=/nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results 
 wait
-transrate --assembly /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/Trinity.fasta --left ${LOC_PAIR}/1F_fastq-q-filt.fq,${LOC_PAIR}/2F_fastq-q-filt.fq --right ${LOC_PAIR}/1R_fastq-q-filt.fq,${LOC_PAIR}/2R_fastq-q-filt.fq --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/transrate-eval/paired-end_assembly/
+transrate --assembly /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/Trinity.fasta --left ${LOC_PAIR}/1F_fastq-q-filt_headerchange_sorted.fastq,${LOC_PAIR}/2F_fastq-q-filt_sorted.fastq --right ${LOC_PAIR}/1R_fastq-q-filt_headerchange_sorted.fastq,${LOC_PAIR}/2R_fastq-q-filt_sorted.fastq --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/transrate-eval/paired-end_assembly/
 wait
 echo "Done with script" 
 date
