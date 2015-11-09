787ca892bd7a33e03f744f512a02177b9bdfbdc5
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-11-09 13:53:34 +0100

    Script for running qualimap on BAM file

---
+++ code/Bash-scripts/Qualimap/qualimap_run_bamqc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -cwd
+#$ -q high_mem
+#$ -o stdout_qualimap.txt
+#$ -e stderr_qualimap.txt
+#$ -j y
+#$ -S /bin/bash
+wait
+/home/mathias/qualimap_v2.1.3/qualimap bamqc -bam /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/align_and_estimate_abundance/bowtime.csorted.bam.ok -outdir /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/align_and_estimate_abundance/qualimap_results
+wait
+echo "Done with script" 
+date
+
