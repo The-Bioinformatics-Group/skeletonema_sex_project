fb92f42837bfb99931ea19028ed81f5cbee0ff66
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-11-11 13:17:13 +0100

    not enough available resources

--- code/Bash-scripts/Trinity-bash-scripts/trinity_run_pairedend.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_run_pairedend.sh
@@ -5,11 +5,11 @@
 #$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_sorted+readnorm_trinity/stderr_trinity.txt
 #$ -j y
 #$ -S /bin/bash
-#$ -l mem_free=240G
+#$ -l mem_free=140G
 wait
 LOC_PAIR=/nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results
 wait 
-Trinity --seqType fq --max_memory 240G --SS_lib_type FR --left ${LOC_PAIR}/1F_sorted+singletons.fq,${LOC_PAIR}/2F_sorted+singletons.fq --right ${LOC_PAIR}/1R_sorted+singletons.fq,${LOC_PAIR}/2R_sorted+singletons.fq --CPU 24 --bflyHeapSpaceInit 10G --bflyHeapSpaceMax 12G --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_sorted+readnorm_trinity --normalize_max_read_cov 40
+Trinity --seqType fq --max_memory 140G --SS_lib_type FR --left ${LOC_PAIR}/1F_sorted+singletons.fq,${LOC_PAIR}/2F_sorted+singletons.fq --right ${LOC_PAIR}/1R_sorted+singletons.fq,${LOC_PAIR}/2R_sorted+singletons.fq --CPU 14 --bflyHeapSpaceInit 10G --bflyHeapSpaceMax 12G --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_sorted+readnorm_trinity --normalize_max_read_cov 40
 wait
 echo "Done with script" 
 date
