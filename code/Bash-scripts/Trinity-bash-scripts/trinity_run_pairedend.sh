4f7e19927779d092310667cc18949491959c3e16
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-11-11 13:15:25 +0100

    a test paired end assembly with sorted reads

--- code/Bash-scripts/Trinity-bash-scripts/trinity_run_pairedend.sh
+++ code/Bash-scripts/Trinity-bash-scripts/trinity_run_pairedend.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly+readnorm_trinity/stdout_trinity.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly+readnorm_trinity/stderr_trinity.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_sorted+readnorm_trinity/stdout_trinity.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_sorted+readnorm_trinity/stderr_trinity.txt
 #$ -j y
 #$ -S /bin/bash
 #$ -l mem_free=240G
 wait
 LOC_PAIR=/nobackup/data5/skeletonema_sex_project/test/data-test/skeletonema-pairend-data/fastq_quality_filter_results
 wait 
-Trinity --seqType fq --max_memory 240G --SS_lib_type FR --left ${LOC_PAIR}/1F_fastq-q-filt.txt,${LOC_PAIR}/2F_fastq-q-filt.txt --right ${LOC_PAIR}/1R_fastq-q-filt.txt,${LOC_PAIR}/2R_fastq-q-filt.txt --CPU 24 --bflyHeapSpaceInit 10G --bflyHeapSpaceMax 12G --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly+readnorm_trinity --normalize_max_read_cov 40
+Trinity --seqType fq --max_memory 240G --SS_lib_type FR --left ${LOC_PAIR}/1F_sorted+singletons.fq,${LOC_PAIR}/2F_sorted+singletons.fq --right ${LOC_PAIR}/1R_sorted+singletons.fq,${LOC_PAIR}/2R_sorted+singletons.fq --CPU 24 --bflyHeapSpaceInit 10G --bflyHeapSpaceMax 12G --bflyCalculateCPU --output /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_sorted+readnorm_trinity --normalize_max_read_cov 40
 wait
 echo "Done with script" 
 date
