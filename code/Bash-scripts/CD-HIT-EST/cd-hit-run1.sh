a518da4e5495b938f9312c23ced95ed0c7626bdb
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-11-22 20:19:33 +0100

    changed script to run for mega-assembly,dedupe1

--- code/Bash-scripts/CD-HIT-EST/cd-hit-run1.sh
+++ code/Bash-scripts/CD-HIT-EST/cd-hit-run1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly3/stdout_CD-HIT-EST.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly3/stderr_CD-HIT-EST.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity/treated-assemblies/cd-hit-est-dedupe1/cd-hit-est1/stdout_CD-HIT-EST.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity/treated-assemblies/cd-hit-est-dedupe1/cd-hit-est1/stderr_CD-HIT-EST.txt
 #$ -j y
 #$ -S /bin/bash
 wait
 SOFTWARE=/nobackup/data5/skeletonema_sex_project/software/cdhit-master/cd-hit-est
-ASSEMBLY=/nobackup/data5/skeletonema_sex_project/test/assembly-test/singleend+pairedend-trinity.fasta
-OUTPUT=/nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly3
+ASSEMBLY=/nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity/treated-assemblies/cd-hit-est-dedupe1/cd-hit-est1/mega-assembly-dedupe1.fq
+OUTPUT=/nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity/treated-assemblies/cd-hit-est-dedupe1/cd-hit-est1/
 wait
-$SOFTWARE -i $ASSEMBLY -o $OUTPUT/merged_settings3.fasta -c 0.85 -n 8 -s 0.80 -T 4 -M 20000 
+$SOFTWARE -i $ASSEMBLY -o $OUTPUT/merged_settings1.fasta -c 0.99 -n 10 -s 0.90 -T 4 -M 20000 
 wait
 echo "Done with script" 
 date
