89398741efad400173182b33f03a0857fb4ae7ee
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-11-23 14:09:49 +0100

    dedupe_run1 for merged assembly

--- code/Bash-scripts/Dedupe/dedupe_run.sh
+++ code/Bash-scripts/Dedupe/dedupe_run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #$ -cwd
-#$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity/treated-assemblies/stdout_dedupe.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity/treated-assemblies/stderr_dedupe.txt
+#$ -q node0
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly/dedupe_run1/stdout_dedupe.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly/dedupe_run1/stderr_dedupe.txt
 #$ -j y
 #$ -S /bin/bash
 wait
 SOFTWARE=/nobackup/data5/skeletonema_sex_project/software/bbmap/dedupe.sh
-ASSEMBLY=/nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity
-OUTPUT=/nobackup/data5/skeletonema_sex_project/test/assembly-test/mega-assembly_trinity/treated-assemblies
+ASSEMBLY=/nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly/singleend+pairedend-trinity.fasta
+OUTPUT=/nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly/dedupe_run1
 wait
-$SOFTWARE in=$ASSEMBLY/Trinity.fasta out=$OUTPUT/mega-assembly-dedupe1.fq t=10 
+$SOFTWARE in=$ASSEMBLY out=$OUTPUT/mega-assembly-dedupe1.fq t=4
 wait
 echo "Done with script" 
 date
