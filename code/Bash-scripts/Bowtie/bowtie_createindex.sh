dfb5b321b1771203e13dd2b757c94659de2e2607
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-11-26 14:06:04 +0100

    bowtie used to create index of pairedendassembly

--- code/Bash-scripts/Bowtie/bowtie_createindex.sh
+++ code/Bash-scripts/Bowtie/bowtie_createindex.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/merged_assemblies/bowtieindex/stdout_bowtieindex.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/merged_assemblies/bowtieindex/stderr_bowtieindex.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/bowtie_index/stdout_bowtieindex.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/bowtie_index/stderr_bowtieindex.txt
 #$ -j y
 #$ -S /bin/bash
 wait
-ASSEMBLY=/nobackup/data5/skeletonema_sex_project/test/assembly-test/merged_assemblies/singleend+pairedend-trinity.fasta
+ASSEMBLY=/nobackup/data5/skeletonema_sex_project/test/assembly-test/paired_end_assembly/Trinity.fasta
 wait
-bowtie2-build $ASSEMBLY merged_assemblies-bowtie2-ref 
+bowtie2-build $ASSEMBLY pairedend-assembly-bowtie2-ref 
 wait
 echo "Done with script" 
 date
