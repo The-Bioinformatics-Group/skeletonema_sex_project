b5feaf4b6bffd8f08b9917e3104a97f7be6407f0
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-12-01 14:15:17 +0100

    changed to run for FilteredModels_transcripts_Thaps3_geneModels_FilteredModels2_nt.fasta

--- code/Bash-scripts/Detonate-Transcriptome-Eval/rsem-transcriptdistr.sh
+++ code/Bash-scripts/Detonate-Transcriptome-Eval/rsem-transcriptdistr.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution/stdout_transceval2.txt
-#$ -e /home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution/stderr_transceval2.txt
+#$ -o /home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution/stdout_transceval3.txt
+#$ -e /home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution/stderr_transceval3.txt
 #$ -j y
 #$ -S /bin/bash
 wait
 SOFTWARE=/home/mathias/detonate-1.10/rsem-eval/rsem-eval-estimate-transcript-length-distribution
 PARFILELOC=/home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution
-TRANSCRIPTOME=/nobackup/data5/data/references/Heterokonts_genomes_transcriptoms_JGI/transcriptoms/Thalassiosira_rotula_conditionsforcounting/de_novo_assembly/Thalassiosira_rotula_final_contigs.fa
+TRANSCRIPTOME=/nobackup/data5/data/references/Heterokonts_genomes_transcriptoms_JGI/Allgenomes/Thalassiosira_pseudonana/FilteredModels_transcripts_Thaps3_geneModels_FilteredModels2_nt.fasta
 wait 
-$SOFTWARE $TRANSCRIPTOME $PARFILELOC/thalassiosira_rotula_conditionsforcounting_denovo_final.txt
+$SOFTWARE $TRANSCRIPTOME $PARFILELOC/thalassiosira_psuedonana_filtmodelthaps3genemodels2.txt
 wait
 echo "Done with script" 
 date
