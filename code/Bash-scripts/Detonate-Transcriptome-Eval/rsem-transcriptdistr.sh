9da44ddf5de8e6807438328911fdeced1bb35351
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-12-01 14:04:01 +0100

    changed to estimate conditionsforcounting thalassiosira rotula assembly

--- code/Bash-scripts/Detonate-Transcriptome-Eval/rsem-transcriptdistr.sh
+++ code/Bash-scripts/Detonate-Transcriptome-Eval/rsem-transcriptdistr.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution/stdout_transceval.txt
-#$ -e /home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution/stderr_transceval.txt
+#$ -o /home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution/stdout_transceval2.txt
+#$ -e /home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution/stderr_transceval2.txt
 #$ -j y
 #$ -S /bin/bash
 wait
 SOFTWARE=/home/mathias/detonate-1.10/rsem-eval/rsem-eval-estimate-transcript-length-distribution
 PARFILELOC=/home/mathias/detonate-1.10/rsem-eval/true_transcript_length_distribution
-TRANSCRIPTOME=/nobackup/data5/data/references/Heterokonts_genomes_transcriptoms_JGI/transcriptoms/Thalassiosira_rotula_transcriptome_poolforassembly/de_novo_assembly/thalassiosira_rotula_final_contigs.fa
+TRANSCRIPTOME=/nobackup/data5/data/references/Heterokonts_genomes_transcriptoms_JGI/transcriptoms/Thalassiosira_rotula_conditionsforcounting/de_novo_assembly/Thalassiosira_rotula_final_contigs.fa
 wait 
-$SOFTWARE $TRANSCRIPTOME $PARFILELOC/thalassiosira_rotula_poolforassembly_denovo_final.txt
+$SOFTWARE $TRANSCRIPTOME $PARFILELOC/thalassiosira_rotula_conditionsforcounting_denovo_final.txt
 wait
 echo "Done with script" 
 date
