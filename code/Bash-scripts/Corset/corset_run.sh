f4b2d8718eb0c054693ea2e229eef5e16d07ca8b
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-12-07 14:23:19 +0100

    running corset to align all data to the transcriptome

--- code/Bash-scripts/Corset/corset_run.sh
+++ code/Bash-scripts/Corset/corset_run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/mega-assembly_corset/stdout_corset.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/mega-assembly_corset/stderr_corset.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly_3inone/cd-hit-est/4/transrate/3merged_settings4/bowtie_aligned/corset_output/stdout_corset.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly_3inone/cd-hit-est/4/transrate/3merged_settings4/bowtie_aligned/corset_output/stderr_corset.txt
 #$ -j y
 #$ -S /bin/bash
 wait
 SOFTWARE=/nobackup/data5/skeletonema_sex_project/software/corset/Corset-version-1.04/corset
-ASSEMBLY=/nobackup/data5/skeletonema_sex_project/test/assembly-test/singleend+pairedend-trinity.fasta
-OUTPUT=/nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly4
-BAMFILES=/nobackup/data5/skeletonema_sex_project/test/assembly-test/read-mapping_mega-assembly
+ASSEMBLY=/nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly_3inone/cd-hit-est/4/transrate/3merged_settings4/transrate-good+rsem-good.fasta
+OUTPUT=/nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly_3inone/cd-hit-est/4/transrate/3merged_settings4/bowtie_aligned/corset_output
+BAMFILES=/nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly_3inone/cd-hit-est/4/transrate/3merged_settings4/bowtie_aligned
 S=alignment.sam
-P=alignment_fast.sam
+P=alignment.sam
 wait
-$SOFTWARE -g CL2-17BI,CL2-17BI,CL3-17BI,CL3-17BI,CS2-17AS,CS2-17CS,CS2-17DS,CS3-17AS,CS3-17CS,CS3-17DS,NCL3-17BI,NCL3-17BI,NCL2-17BI,NCL2-17BI,pairedreads1,pairedreads2 -n CL2-17BI-REP1,CL2-17BI-REP2,CL3-17BI-REP1,CL3-17BI-REP2,CS2-17AS,CS2-17CS,CS2-17DS,CS3-17AS,CS3-17CS,CS3-17DS,NCL3-17BI-REP1,NCL3-17BI-REP2,NCL2-17BI-REP1,NCL2-17BI-REP2,pairedreads1,pairedreads2  -p corset_mega-assembly $BAMFILES/sample1/$S $BAMFILES/sample2/$S $BAMFILES/sample3/$S $BAMFILES/sample4/$S $BAMFILES/sample5/$S $BAMFILES/sample6/$S $BAMFILES/sample7/$S $BAMFILES/sample8/$S $BAMFILES/sample9/$S $BAMFILES/sample10/$S $BAMFILES/sample11/$S $BAMFILES/sample12/$S $BAMFILES/sample13/$S $BAMFILES/sample14/$S $BAMFILES/sample1_pairedend/$P $BAMFILES/sample2_pairedend/$P
+$SOFTWARE -g CL2-17Bl,CL2-17Bl,CL3-17Bl,CL3-17Bl,CS2,CS2,CS2,CS3,CS3,CS3,NCL3-17Bl,NCL3-17Bl,NCL2-17Bl,NCL2-17Bl,P1,P2 -n CL2-17BI-REP1,CL2-17BI-REP2,CL3-17BI-REP1,CL3-17BI-REP2,CS2-17AS,CS2-17CS,CS2-17DS,CS3-17AS,CS3-17CS,CS3-17DS,NCL3-17BI-REP1,NCL3-17BI-REP2,NCL2-17BI-REP1,NCL2-17BI-REP2,pairedreads1,pairedreads2  -p corset_clusteredcontigs $BAMFILES/sample1/$S $BAMFILES/sample2/$S $BAMFILES/sample3/$S $BAMFILES/sample4/$S $BAMFILES/sample5/$S $BAMFILES/sample6/$S $BAMFILES/sample7/$S $BAMFILES/sample8/$S $BAMFILES/sample9/$S $BAMFILES/sample10/$S $BAMFILES/sample11/$S $BAMFILES/sample12/$S $BAMFILES/sample13/$S $BAMFILES/sample14/$S $BAMFILES/sample1_p/$P $BAMFILES/sample2_p/$P
 wait
 echo "Done with script" 
 date
