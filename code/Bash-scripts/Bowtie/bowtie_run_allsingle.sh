5671619d9d77f5c789cab9a23732d8c6a1d22c91
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-12-11 14:36:21 +0100

    bowtie2 alignment of single end reads to transcriptome

--- code/Bash-scripts/Bowtie/bowtie_run_allsingle.sh
+++ code/Bash-scripts/Bowtie/bowtie_run_allsingle.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly_3inone/cd-hit-est/4/transrate/3merged_settings4/bowtie_aligned/stdout_bowtie_single.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly_3inone/cd-hit-est/4/transrate/3merged_settings4/bowtie_aligned/stderr_bowtie_single.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/bowtie2-read-mapping/stdout_bowtie.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/bowtie2-read-mapping/stderr_bowtie.txt
 #$ -j y
 #$ -S /bin/bash
 wait
-ASSEMBLY=/nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly_3inone/cd-hit-est/4/transrate/3merged_settings4/bowtie_aligned/transrate-good+rsem-good_index
-OUTPUT=/nobackup/data5/skeletonema_sex_project/test/assembly-test/large-assemblies+eval/merged-assembly_3inone/cd-hit-est/4/transrate/3merged_settings4/bowtie_aligned
+ASSEMBLY=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/bowtie2-read-mapping/transcriptome-bowtie2index
+OUTPUT=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/bowtie2-read-mapping
 DATA=/nobackup/data5/skeletonema_sex_project/test/data-test/rna-sex/fastq_quality_filter_results
 TEMP=/nobackup/data5/skeletonema_sex_project/test/temporary_files/rnasex-bowtie.txt
 NUM=0
@@ -24,12 +24,14 @@ NUM=$((NUM+1))
 wait
 RNAFILE=$(sed "${NUM}q;d" $TEMP)
 wait
-echo "Mapping reads from $RNAFILE to mega-assembly"
+echo "Mapping reads from $RNAFILE to s-marinoi transcriptome"
 OUTPUTSPEC=$DATA/$RNAFILE
+wait
+mkdir $OUTPUT/sample${NUM}
 wait
 touch $OUTPUT/sample${NUM}/alignment.sam
 wait
-bowtie2 -a --end-to-end --threads 20 -x $ASSEMBLY/3mergedcdhitest4transdetofiltered-index -U $OUTPUTSPEC -S $OUTPUT/sample${NUM}/alignment.sam
+bowtie2 -a --end-to-end --threads 20 -x $ASSEMBLY/s-marinoi_bowtie2_transcriptomeindex -U $OUTPUTSPEC -S $OUTPUT/sample${NUM}/alignment.sam
 wait
 done
 wait
