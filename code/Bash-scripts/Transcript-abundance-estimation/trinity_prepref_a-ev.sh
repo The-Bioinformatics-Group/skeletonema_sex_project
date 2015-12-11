b3dfb241c6e5b3bb9d4c094ed1ea0a6b7fcf7534
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-12-11 16:51:54 +0100

    eXpress not installed, using RSEM instead

--- code/Bash-scripts/Transcript-abundance-estimation/trinity_prepref_a-ev.sh
+++ code/Bash-scripts/Transcript-abundance-estimation/trinity_prepref_a-ev.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #$ -cwd
 #$ -q high_mem
-#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcript-abundance-est/bowtie2-mapping1/stdout_express_prepref.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcript-abundance-est/bowtie2-mapping1/stderr_express_prepref.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcript-abundance-est/bowtie2-mapping1/stdout_RSEM_prepref.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcript-abundance-est/bowtie2-mapping1/stderr_RSEM_prepref.txt
 #$ -j y
 #$ -S /bin/bash
 wait
 ASSEMBLY=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/skeletonema-marinoi_transcriptome_unannotated.fasta
 OUTPUT=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcript-abundance-est/bowtie2-mapping1
 wait 
-/usr/local/bin/trinityrnaseq_r20140717/util/align_and_estimate_abundance.pl --transcripts $ASSEMBLY --est_method eXpress --aln_method bowtie2 --prep_reference --output_dir $OUTPUT 
+/usr/local/bin/trinityrnaseq_r20140717/util/align_and_estimate_abundance.pl --transcripts $ASSEMBLY --est_method RSEM --aln_method bowtie2 --prep_reference --output_dir $OUTPUT 
 wait
 echo "Done with script" 
 date
