d54191b0148dd89106a47332748d1bc0e3786242
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-12-29 14:08:20 +0100

    BLAST_run1

---
+++ code/Bash-scripts/BLAST/BLAST_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -cwd
+#$ -q node0
+#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes/stdout_BLAST.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes/stderr_BLAST.txt
+#$ -j y
+#$ -S /bin/bash
+wait
+ANALYSISLOC=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes
+TRANSCRIPTOME=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/skeletonema-marinoi_transcriptome_unannotated.fasta
+wait
+tblastn -query $ANALYSISLOC/meiosis_athal+scerv.fa -subject $TRANSCRIPTOME -matrix BLOSUM62 -evalue 0.1 -gapopen 11 -gapextend 1 -word_size 6 -out $ANALYSISLOC/blastresults.txt -max_target_seqs 20
+wait
+echo "Done with script" 
+date
