9e700dd2f37e7e500614ed08dc5506d9d7217690
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2015-12-29 15:09:33 +0100

    blastrun 3, using database, only 5 alignments

--- code/Bash-scripts/BLAST/BLAST_run.sh
+++ code/Bash-scripts/BLAST/BLAST_run.sh
@@ -9,7 +9,7 @@ wait
 ANALYSISLOC=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes
 TRANSCRIPTOME=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcritpome-blast_db/skeletonema-marinoi_transcriptome_unannotated.fasta
 wait
-tblastn -query $ANALYSISLOC/meiosis_athal+scerv.fa -db skeletonema-marinoi_transcriptome_blastdb/skeletonema-marinoi_transcriptome_unannotated.fasta -matrix BLOSUM62 -evalue 0.1 -gapopen 11 -gapextend 1 -word_size 6 -out $ANALYSISLOC/blastresults3.txt -num_threads 10 
+tblastn -query $ANALYSISLOC/meiosis_athal+scerv.fa -db skeletonema-marinoi_transcriptome_blastdb/skeletonema-marinoi_transcriptome_unannotated.fasta -max_target_seqs 5 -matrix BLOSUM62 -evalue 0.1 -gapopen 11 -gapextend 1 -word_size 6 -out $ANALYSISLOC/blastresults_only5.txt -num_threads 10 
 wait
 echo "Done with script" 
 date
