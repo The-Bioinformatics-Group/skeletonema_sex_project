336f3bfc63be038c95b9c0768c89a36272afce95
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-01-13 12:54:21 +0100

    Updated to run in ncbi database

---
+++ code/Bash-scripts/BLAST/BLASTX_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -cwd
+#$ -q node0
+#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes/ncbi-results/stdout_BLASTX.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes/ncbi-results/stderr_BLASTX.txt
+#$ -j y
+#$ -S /bin/bash
+wait
+ANALYSISLOC=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes/ncbi-results
+wait
+blastx -db /state/partition1/db/ncbi/nr -query ${ANALYSISLOC}/contigsforreciprocblast.txt -evalue 0.001 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 0 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC/ncbi_results_outfmt0.txt
+wait
+blastx -db /state/partition1/db/ncbi/nr -query ${ANALYSISLOC}/contigsforreciprocblast.txt -evalue 0.001 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 7 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC/ncbi_results_outfmt7.txt
+
+wait
+echo "Done with script" 
+date
