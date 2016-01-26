0fd6c4b627d243fe7c148822ff1aec72d5c7eb43
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-01-26 22:41:02 +0100

    Updated script run tblastn against transcriptome with flagellagenes as query

--- code/Bash-scripts/BLAST/BLAST_run.sh
+++ code/Bash-scripts/BLAST/BLAST_run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #$ -cwd
 #$ -q node0
-#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes/stdout_BLAST.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes/stderr_BLAST.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/flagellagenes/stdout_BLAST.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/flagellagenes/stderr_BLAST.txt
 #$ -j y
 #$ -S /bin/bash
 wait
-ANALYSISLOC=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes
+ANALYSISLOC=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/flagellagenes
 TRANSCRIPTOME=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcritpome-blast_db/skeletonema-marinoi_transcriptome_unannotated.fasta
 wait
-tblastn -query $ANALYSISLOC/meiosis_athal+scerv.fa -db skeletonema-marinoi_transcriptome_blastdb/skeletonema-marinoi_transcriptome_unannotated.fasta -outfmt 7 -matrix BLOSUM62 -evalue 0.1 -gapopen 11 -gapextend 1 -word_size 6 -out $ANALYSISLOC/blastresults_outfmt7.txt -num_threads 10 
+tblastn -query $ANALYSISLOC/flagella_seq.fasta -db skeletonema-marinoi_transcriptome_blastdb/skeletonema-marinoi_transcriptome_unannotated.fasta -outfmt 7 -matrix BLOSUM62 -evalue 0.1 -gapopen 11 -gapextend 1 -word_size 6 -out $ANALYSISLOC/blastresults_outfmt7.txt -num_threads 10 
 wait
 echo "Done with script" 
 date
