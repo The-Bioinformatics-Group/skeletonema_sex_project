eedc80b9bed7d4c8e9fa30a1dc54524b332fdfc3
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-01-27 11:46:24 +0100

    BLASTx of 59gene-transcriptomecontighits against ncbi,nr

--- code/Bash-scripts/BLAST/BLASTX_run.sh
+++ code/Bash-scripts/BLAST/BLASTX_run.sh
@@ -8,9 +8,11 @@
 wait
 ANALYSISLOC=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/59athal+scere-meiosisgenes/ncbi-results
 wait
-blastx -db /state/partition1/db/ncbi/nr -query ${ANALYSISLOC}/contigsforreciprocblast.txt -evalue 0.001 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 0 -show_gis -matrix BLOSUM62 -out $ANALYSISLOC/ncbi_results_outfmt0.txt -num_threads 20
+export BLASTDB=/state/partition1/db/ncbi/nr/
 wait
-blastx -db /state/partition1/db/ncbi/nr -query ${ANALYSISLOC}/contigsforreciprocblast.txt -evalue 0.001 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 7 -show_gis -matrix BLOSUM62 -out $ANALYSISLOC/ncbi_results_outfmt7.txt -num_threads 20
+blastx -db nr -query ${ANALYSISLOC}/contigsforreciprocblast.txt -evalue 0.001 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 0 -show_gis -matrix BLOSUM62 -out $ANALYSISLOC/ncbi_results_outfmt0.txt -num_threads 20
+wait
+blastx -db nr -query ${ANALYSISLOC}/contigsforreciprocblast.txt -evalue 0.001 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 7 -show_gis -matrix BLOSUM62 -out $ANALYSISLOC/ncbi_results_outfmt7.txt -num_threads 20
 wait
 echo "Done with script" 
 date
