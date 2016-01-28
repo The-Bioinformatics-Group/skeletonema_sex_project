49356cad5e28398f67f5af1dfc9589128a7b8403
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-01-28 12:55:00 +0100

    Changed script to run tBLASTn of thalassiosira and phaedatylum annotated proteins against transcriptome

--- code/Bash-scripts/BLAST/BLAST_run.sh
+++ code/Bash-scripts/BLAST/BLAST_run.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 #$ -cwd
 #$ -q node0
-#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/flagellagenes/stdout_BLAST.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/flagellagenes/stderr_BLAST.txt
+#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation/stdout_BLAST.txt
+#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation/stderr_BLAST.txt
 #$ -j y
 #$ -S /bin/bash
 wait
-ANALYSISLOC=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/flagellagenes
+QUERYLOC1=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation/references
+QUERYLOC2=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation/references
 TRANSCRIPTOME=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcritpome-blast_db/skeletonema-marinoi_transcriptome_unannotated.fasta
+OUTPUT=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation
 wait
-tblastn -query $ANALYSISLOC/flagella_seq.fasta -db skeletonema-marinoi_transcriptome_blastdb/skeletonema-marinoi_transcriptome_unannotated.fasta -outfmt 7 -matrix BLOSUM62 -evalue 0.1 -gapopen 11 -gapextend 1 -word_size 6 -out $ANALYSISLOC/blastresults_outfmt7.txt -num_threads 10 
+tblastn -query $QUERYLOC1/thalassiosira_pseudonana_proteinannotation.fasta -db skeletonema-marinoi_transcriptome_blastdb/skeletonema-marinoi_transcriptome_unannotated.fasta -outfmt 7 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_thal.txt -num_threads 10 
+wait
+tblastn -query $QUERYLOC1/thalassiosira_pseudonana_proteinannotation.fasta -db skeletonema-marinoi_transcriptome_blastdb/skeletonema-marinoi_transcriptome_unannotated.fasta -outfmt 0 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_thal.txt -num_threads 10
+wait
+tblastn -query $QUERYLOC2/phaeodactylum_tricornutum_proteinannotation.fasta -db skeletonema-marinoi_transcriptome_blastdb/skeletonema-marinoi_transcriptome_unannotated.fasta -outfmt 7 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_phaeoda.txt -num_threads 10 
+wait
+tblastn -query $QUERYLOC2/phaeodactylum_tricornutum_proteinannotation.fasta -db skeletonema-marinoi_transcriptome_blastdb/skeletonema-marinoi_transcriptome_unannotated.fasta -outfmt 0 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_phaeoda.txt -num_threads 10
 wait
 echo "Done with script" 
 date
