c8501db405d1357bb68f62a184c47f74ebf93537
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-01-29 12:09:48 +0100

    changed to run blastx of flagella and meiosis genes against uniprot, output both outfmt7 and 0_corrected

--- code/Bash-scripts/BLAST/BLASTX_run2.sh
+++ code/Bash-scripts/BLAST/BLASTX_run2.sh
@@ -7,15 +7,15 @@
 #$ -S /bin/bash
 wait
 ANALYSISLOC1=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/58athal+scere-meiosisgenes
-ANALYSISLOC2=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/flagellagenes/uniprot_results
+ANALYSISLOC2=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/blast_searches/flagellagenes
 wait
-blastx -db uniprot -query ${ANALYSISLOC1}/58meiosisgenes_contighits.fasta -evalue 0.01 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 7 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC1/uniprot_results_outfmt7_meiosisgenes.txt
+blastx -db swissprot -query ${ANALYSISLOC1}/58meiosisgenes_contighits.fasta -evalue 0.01 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 7 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC1/uniprot_results_outfmt7_meiosisgenes.txt
 wait
-blastx -db uniprot -query ${ANALYSISLOC1}/58meiosisgenes_contighits.fasta -evalue 0.01 -num_alignments 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 0 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC1/uniprot_results_outfmt0_meiosisgenes.txt
+blastx -db swissprot -query ${ANALYSISLOC1}/58meiosisgenes_contighits.fasta -evalue 0.01 -num_alignments 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 0 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC1/uniprot_results_outfmt0_meiosisgenes.txt
 wait
-blastx -db uniprot -query ${ANALYSISLOC2}/flagellagenes_contighits.fasta -evalue 0.01 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 7 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC2/uniprot_results_outfmt7_flagellagenes.txt
+blastx -db swissprot -query ${ANALYSISLOC2}/flagellagenes_contighits.fasta -evalue 0.01 -max_target_seqs 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 7 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC2/uniprot_results_outfmt7_flagellagenes.txt
 wait
-blastx -db uniprot -query ${ANALYSISLOC2}/flagellagenes_contighits.fasta -evalue 0.01 -num_alignments 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 0 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC2/uniprot_results_outfmt0_flagellagenes.txt
+blastx -db swissprot -query ${ANALYSISLOC2}/flagellagenes_contighits.fasta -evalue 0.01 -num_alignments 5 -word_size 6 -gapopen 11 -gapextend 1 -outfmt 0 -show_gis -remote -matrix BLOSUM62 -out $ANALYSISLOC2/uniprot_results_outfmt0_flagellagenes.txt
 wait
 echo "Done with script" 
 date
