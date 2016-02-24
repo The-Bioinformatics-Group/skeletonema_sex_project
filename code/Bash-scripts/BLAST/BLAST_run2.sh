8d5256ed2c4a97302bfe0d0cfac64ccfc03b4fd6
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-02-24 17:43:48 +0100

    Script runs blastx, transcriptome query against thala and phaeda proteome for later use in annotation

--- code/Bash-scripts/BLAST/BLAST_run2.sh
+++ code/Bash-scripts/BLAST/BLAST_run2.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 #$ -cwd
 #$ -q node0
-#$ -o /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation/transcriptome-vs-proteinref/stdout_BLASTx_annotation.txt
-#$ -e /nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation/transcriptome-vs-proteinref/stderr_BLASTx_annotation.txt
+#$ -o /state/partition6/mathias_temp/stdout_BLASTx_annotation.txt
+#$ -e /state/partition6/mathias_temp/stderr_BLASTx_annotation.txt
 #$ -j y
 #$ -S /bin/bash
 wait
-QUERYLOC1=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation/references/thal_database/thalassiosira_pseudonana_proteinannotation.fasta
-QUERYLOC2=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation/references/phaeda_database/phaeodactylum_tricornutum_proteinannotation.fasta
-TRANSCRIPTOME=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome-blast_db/skeletonema-marinoi_transcriptome_unannotated.fasta
-OUTPUT=/nobackup/data5/skeletonema_sex_project/differential-expression-analysis/transcriptome/transcriptome_annotation/transcriptome-vs-proteinref
+DBLOC1=/state/partition6/mathias_temp/references/thal_database/thalassiosira_pseudonana_proteinannotation.fasta
+DBLOC2=/state/partition6/mathias_temp/references/phaeda_database/phaeodactylum_tricornutum_proteinannotation.fasta
+TRANSCRIPTOME=/state/partition6/mathias_temp/skeletonema-marinoi_transcriptome_unannotated.fasta
+OUTPUT=/state/partition6/mathias_temp/blastx_output
 wait
-blastx -query $TRANSCRIPTOME -db $QUERYLOC1 -outfmt 7 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_transcriptomevsthal.txt -num_threads 10 
+blastx -query $TRANSCRIPTOME -db $DBLOC1 -outfmt 7 -max_target_seqs 5 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_transcriptomevsthal.txt -num_threads 40 
 wait
-blastx -query $TRANSCRIPTOME -db $QUERYLOC1 -outfmt 0 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsthal.txt -num_threads 10
+blastx -query $TRANSCRIPTOME -db $DBLOC1 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsthal.txt -num_threads 40
 wait
-blastx -query $TRANSCRIPTOME -db $QUERYLOC2 -outfmt 7 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_transcriptomevsphaeoda.txt -num_threads 10 
+blastx -query $TRANSCRIPTOME -db $DBLOC2 -outfmt 7 -max_target_seqs 5 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_transcriptomevsphaeoda.txt -num_threads 40 
 wait
-blastx -query $TRANSCRIPTOME -db $QUERYLOC2 -outfmt 0 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsphaeoda.txt -num_threads 10
+blastx -query $TRANSCRIPTOME -db $DBLOC2 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsphaeoda.txt -num_threads 40
 wait
 echo "Done with script" 
 date
