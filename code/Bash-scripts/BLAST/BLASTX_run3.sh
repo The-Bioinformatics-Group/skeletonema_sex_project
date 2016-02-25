fcd6a2bf3db35e558a006e44d1ec8cca4cd42860
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-02-25 09:17:59 +0100

    10 cores, blastx of transcriptome against phaeda, outfmt7

---
+++ code/Bash-scripts/BLAST/BLASTX_run3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -cwd
+#$ -q node0
+#$ -o /state/partition6/mathias_temp/stdout_BLASTx_annotation_7ph.txt
+#$ -e /state/partition6/mathias_temp/stderr_BLASTx_annotation_7ph.txt
+#$ -j y
+#$ -S /bin/bash
+wait
+DBLOC1=/state/partition6/mathias_temp/references/thal_database/thalassiosira_pseudonana_proteinannotation.fasta
+DBLOC2=/state/partition6/mathias_temp/references/phaeda_database/phaeodactylum_tricornutum_proteinannotation.fasta
+TRANSCRIPTOME=/state/partition6/mathias_temp/skeletonema-marinoi_transcriptome_unannotated.fasta
+OUTPUT=/state/partition6/mathias_temp/blastx_output
+wait
+blastx -query $TRANSCRIPTOME -db $DBLOC2 -outfmt 7  -max_target_seqs 5 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_transcriptomevsphaeoda.txt -num_threads 10
+wait
+echo "Done with script" 
+date
