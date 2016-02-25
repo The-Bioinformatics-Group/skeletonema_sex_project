1c7527db490f798bd082c506913a49c02a4b25a0
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-02-25 09:20:23 +0100

    changed script, now 10 cores, blastx transcriptome against thalassiosira, outfmt7

--- code/Bash-scripts/BLAST/BLAST_run2.sh
+++ code/Bash-scripts/BLAST/BLAST_run2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -cwd
 #$ -q node0
-#$ -o /state/partition6/mathias_temp/stdout_BLASTx_annotation.txt
-#$ -e /state/partition6/mathias_temp/stderr_BLASTx_annotation.txt
+#$ -o /state/partition6/mathias_temp/stdout_BLASTx_annotation_thal7.txt
+#$ -e /state/partition6/mathias_temp/stderr_BLASTx_annotation_thal7.txt
 #$ -j y
 #$ -S /bin/bash
 wait
@@ -11,13 +11,7 @@ DBLOC2=/state/partition6/mathias_temp/references/phaeda_database/phaeodactylum_t
 TRANSCRIPTOME=/state/partition6/mathias_temp/skeletonema-marinoi_transcriptome_unannotated.fasta
 OUTPUT=/state/partition6/mathias_temp/blastx_output
 wait
-blastx -query $TRANSCRIPTOME -db $DBLOC1 -outfmt 7 -max_target_seqs 5 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_transcriptomevsthal.txt -num_threads 40 
-wait
-blastx -query $TRANSCRIPTOME -db $DBLOC1 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsthal.txt -num_threads 40
-wait
-blastx -query $TRANSCRIPTOME -db $DBLOC2 -outfmt 7 -max_target_seqs 5 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_transcriptomevsphaeoda.txt -num_threads 40 
-wait
-blastx -query $TRANSCRIPTOME -db $DBLOC2 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsphaeoda.txt -num_threads 40
+blastx -query $TRANSCRIPTOME -db $DBLOC1 -outfmt 7 -max_target_seqs 5 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt7_transcriptomevsthal.txt -num_threads 10
 wait
 echo "Done with script" 
 date
