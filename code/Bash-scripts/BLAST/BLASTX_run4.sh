5a76b7c174ba4eaa30e5d485f7c0e3cde62e55e6
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-02-29 10:53:47 +0100

    changed to run on high_mem

--- code/Bash-scripts/BLAST/BLASTX_run4.sh
+++ code/Bash-scripts/BLAST/BLASTX_run4.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #$ -cwd
-#$ -q node0
-#$ -o /state/partition6/mathias_temp/stdout_BLASTx_annotation_thal0.txt
-#$ -e /state/partition6/mathias_temp/stderr_BLASTx_annotation_thal0.txt
+#$ -q high_mem
+#$ -o /state/partition2/mathias_temp/stdout_BLASTx_annotation_thal0.txt
+#$ -e /state/partition2/mathias_temp/stderr_BLASTx_annotation_thal0.txt
 #$ -j y
 #$ -S /bin/bash
 wait
-DBLOC1=/state/partition6/mathias_temp/references/thal_database/thalassiosira_pseudonana_proteinannotation.fasta
-DBLOC2=/state/partition6/mathias_temp/references/phaeda_database/phaeodactylum_tricornutum_proteinannotation.fasta
-TRANSCRIPTOME=/state/partition6/mathias_temp/skeletonema-marinoi_transcriptome_unannotated.fasta
-OUTPUT=/state/partition6/mathias_temp/blastx_output
+DBLOC1=/state/partition2/mathias_temp/references/thal_database/thalassiosira_pseudonana_proteinannotation.fasta
+DBLOC2=/state/partition2/mathias_temp/references/phaeda_database/phaeodactylum_tricornutum_proteinannotation.fasta
+TRANSCRIPTOME=/state/partition2/mathias_temp/skeletonema-marinoi_transcriptome_unannotated.fasta
+OUTPUT=/state/partition2/mathias_temp/blastx_output
 wait
 blastx -query $TRANSCRIPTOME -db $DBLOC1 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsthal.txt -num_threads 10
 wait
