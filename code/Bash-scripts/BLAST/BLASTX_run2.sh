1ef628cac1ed1a24b15069773a9b34f86ddf141a
Mathias Johansson - math.joh.bio@gmail.com <mathias@albiorix.bioenv.gu.se>
2016-03-02 19:33:39 +0100

    Updated blastx scripts to run 4 different runs for 4 different parts of transcriptome

--- code/Bash-scripts/BLAST/BLASTX_run2.sh
+++ code/Bash-scripts/BLAST/BLASTX_run2.sh
@@ -8,10 +8,16 @@
 wait
 DBLOC1=/state/partition2/mathias_temp/references/thal_database/thalassiosira_pseudonana_proteinannotation.fasta
 DBLOC2=/state/partition2/mathias_temp/references/phaeda_database/phaeodactylum_tricornutum_proteinannotation.fasta
-TRANSCRIPTOME=/state/partition2/mathias_temp/skeletonema-marinoi_transcriptome_unannotated.fasta
+TRANSCRIPTOME=/state/partition2/mathias_temp
 OUTPUT=/state/partition2/mathias_temp/blastx_output
 wait
-blastx -query $TRANSCRIPTOME -db $DBLOC2 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsphaeoda.txt -num_threads 10
+blastx -query $TRANSCRIPTOME/skeletonema-marinoi_transcriptome_unannotated_part1.fasta -db $DBLOC2 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsphaeoda_part1.txt -num_threads 8
+wait
+blastx -query $TRANSCRIPTOME/skeletonema-marinoi_transcriptome_unannotated_part2.fasta -db $DBLOC2 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsphaeoda_part2.txt -num_threads 8
+wait
+blastx -query $TRANSCRIPTOME/skeletonema-marinoi_transcriptome_unannotated_part3.fasta -db $DBLOC2 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsphaeoda_part3.txt -num_threads 8
+wait
+blastx -query $TRANSCRIPTOME/skeletonema-marinoi_transcriptome_unannotated_part4.fasta -db $DBLOC2 -outfmt 0 -num_alignments 10 -matrix BLOSUM62 -evalue 0.01 -gapopen 11 -gapextend 1 -word_size 6 -out $OUTPUT/blastresults_outfmt0_transcriptomevsphaeoda_part4.txt -num_threads 8
 wait
 echo "Done with script" 
 date
